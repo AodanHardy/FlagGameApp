1926b3d2efa994e00e3b95b8be081c22b0bd7c75
Aodan <hardy-a4@ulster.ac.uk>
2024-10-04 08:45:29 +0100

    adding country class

---
+++ app/src/main/java/com/example/flaggame/Country.java
@@ -0,0 +1,27 @@
+package com.example.flaggame;
+
+public class Country {
+    private String name;
+    private String imageUrl;
+
+    public Country(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
