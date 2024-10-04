c72b40cb81f307f4048082b3f9f54be2f033e6f5
Aodan <hardy-a4@ulster.ac.uk>
2024-10-04 09:23:51 +0100

    adding flag manager

---
+++ app/src/main/java/com/example/flaggame/FlagManager.java
@@ -0,0 +1,11 @@
+package com.example.flaggame;
+
+import java.util.List;
+
+public class FlagManager {
+    private List<Country> countries;
+
+    public FlagManager() {
+        this.countries.add(new Country("",""));
+    }
+}
