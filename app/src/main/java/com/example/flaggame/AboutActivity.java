140e9f00a9e1b1a9f24dd2a23d9092bd6d23b37e
Aodan <hardy-a4@ulster.ac.uk>
2024-10-08 15:18:55 +0100

    home page and about created

---
+++ app/src/main/java/com/example/flaggame/AboutActivity.java
@@ -0,0 +1,29 @@
+package com.example.flaggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutActivity extends AppCompatActivity {
+    Button backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        backBtn = findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/flaggame/MainActivity.java
+++ app/src/main/java/com/example/flaggame/MainActivity.java
@@ -2,18 +2,41 @@ package com.example.flaggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.List;
-
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
+    Button aboutBtn;
+    Button newGameBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutBtn = findViewById(R.id.aboutBtn);
+
+        newGameBtn = findViewById(R.id.newCountryGameBtn);
+
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
 
     }
 }
